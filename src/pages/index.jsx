b84d3ea6d73e866863a5dcdbd73abd1f350510f7
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-11-15 16:31:46 +0000

    new react app

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const MainPage = () => {
+  return (
+    <div>
+      <h3>Welcome To the React router</h3>
+      <small>Main Page</small>
+    </div>
+  );
+}
+
+export default MainPage;
