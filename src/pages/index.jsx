b5223cc794391d3699c8c54331cddae7b118399e
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-11-15 17:04:29 +0000

    Created random array of letters

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,3 +10,13 @@ const MainPage = () => {
 }
 
 export default MainPage;
+
+// Creating an array
+const NewArray = () => {
+  // const newArray = array.map((x) => String.fromCharCode(x));
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  return Array.from({
+    length: 10
+  }, () => alphabet[Math.floor(Math.random() * alphabet.length)])
+}
+console.log(NewArray());
